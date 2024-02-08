f23e721c2dc9542ca35fc3716972fcf097d5c694
Mtoulliou <matteo.meze@gmail.com>
2024-02-08 08:50:48 +0100

    premier affichage web

--- TCP/listings/views.py
+++ TCP/listings/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def accueil(request):
+    return HttpResponse('<h1>TCP</h1>\n<h2>Transport Colis Passion</h2>')
