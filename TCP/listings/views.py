87e6aadc3507f0c33e98927ed4b183ab9b5badb6
Mtoulliou <matteo.meze@gmail.com>
2024-02-08 09:16:15 +0100

    premier fichier html

--- TCP/listings/views.py
+++ TCP/listings/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def accueil(request):
-    return HttpResponse('<h1>TCP</h1>\n<h2>Transport Colis Passion</h2>')
+    with open("pages/index.html", 'r') as file:
+        html_content = file.read()
+    return HttpResponse(html_content, content_type='text/html')
