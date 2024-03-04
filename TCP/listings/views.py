24e0de5b479c2947cef78f5ccf1fadd009306552
Mtoulliou <matteo.meze@gmail.com>
2024-03-04 14:32:00 +0100

    Modif quelques erreurs

--- TCP/listings/views.py
+++ TCP/listings/views.py
@@ -5,3 +5,12 @@ from listings.models import AdminTCP
 def accueil(request):
     admintcp=AdminTCP.objects.all()
     return render(request, 'listings/index.html',{'admintcp':admintcp})
+
+def destinataire(request):
+    return render(request, 'listings/destinataire.html')
+
+def expediteur(request):
+    return render(request, 'listings/expediteur.html')      
+
+def transporteur(request): 
+    return render(request, 'listings/transporteur.html')    
